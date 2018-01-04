69826467bd730da9d384bec25582f23fda4e1aa8
Kate Nepovinnykh <kate.nepovinnykh@graph.uk>
2018-01-04 22:49:58 +0500

    Move all files to root

--- app/components/wish-form.js
+++ app/components/wish-form.js
@@ -1,9 +1,11 @@
-import Component from '@ember/component'
+import Component from '@ember/component';
+import maleWish from '../../wishes/male.js';
 
 export default Component.extend({
 	actions: {
 		add(x) {
 			const gender  = this.getProperties('gender').gender;
+			console.log(maleWish);
 
 			if (gender === 'male') {
 				const wish = Ember.$.getJSON("../../wishes/male.json");
